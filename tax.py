825faf069cae011d14a6ee22d04f465b8b4ebba7
Kent Gatera <kent.gatera@stu.ocsb.ca>
2022-10-04 02:49:26 +0000

    made a program in which Tax is calculated using price from user input.

--- tax.py
+++ tax.py
@@ -4,19 +4,23 @@
 # Tax is calculated using price from user input.
 
 
-# The HST tax in British Columbia is 5%.
-HST = 0.05
+def main():
+    # The HST tax in British Columbia is 5%.
+    HST = 0.05
+    # get input of total price.
+    total_price = float(input("What is the price of your item? : " + "$"))
 
-# get input of total price.
-total_price = float(input("What is the price of your item? : " + "$"))
-
-tax = HST * total_price
-float(tax)
-print("{:.2f}".format(tax))
-total_cost = total_price + tax
-# Add the total price and tax for the total cost.
-print(
-    "The total price is ${0:.2f} + ${1:.2f} = ${2:.2f}".format(
-        total_price, tax, total_cost
+    tax = HST * total_price
+    float(tax)
+    print("{:.2f}".format(tax))
+    total_cost = total_price + tax
+    # Add the total price and tax for the total cost.
+    print(
+        "The total price is ${0:.2f} + ${1:.2f} = ${2:.2f}".format(
+            total_price, tax, total_cost
+        )
     )
-)
+
+
+if __name__ == "__main__":
+    main()
